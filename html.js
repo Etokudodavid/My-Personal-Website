dc819ef688aeb86ba5ca06d1756fa3e61d551fca
Etokudodavid <davidetokudo@gmail.com>
2024-01-07 18:24:25 +0100

    Edited the second image

---
+++ html.js
@@ -0,0 +1,15 @@
+html.js
+// Challenge 2:
+// Replace the arguments below according to your preference.
+
+// space, scary, military, romantic, cowboy, fantasy, superhero
+favouriteMovieGenre("romantic")
+
+// watermelon, tomato, banana, orange, avocado, blueberry
+favouriteFruit("banana")
+
+// light, dark
+favouriteMode("dark")
+
+// sharp, soft, round
+favouriteEdgeStyle("round")
